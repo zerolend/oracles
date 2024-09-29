506a781a45b363a8e727af3e1c6640b505febe68
Deadshot Ryker <ryker@zerolend.xyz>
2024-09-29 23:03:47 +0400

    wrote the oracle for USDz/USDC

---
+++ contracts/aerodrome/implementations/ZAIUSDCvAMMOracle.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {AerodromeVAMMOracle} from "../AerodromeVAMMOracle.sol";
+
+contract ZAIUSDCvAMMOracle is AerodromeVAMMOracle {
+    constructor()
+        AerodromeVAMMOracle(
+            0x72d509aFF75753aAaD6A10d3EB98f2DBC58C480D, // USDz/USDC LP Pool
+            0x78Ad3d53045b6582841e2a1a688C52Be2CA2A7a7, // USDz Fixed Price Feed
+            0x7e860098F58bBFC8648a4311b374B1D669a2bc6B, // USDC Chainlink Feed
+            1e10
+        )
+    {
+        // nothing
+    }
+}
