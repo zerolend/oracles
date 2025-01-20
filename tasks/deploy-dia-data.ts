5b540ff65a5faf847ba21a18b94fb31a5b5d9b9c
obchain <riteishnikhoria@gmail.com>
2025-01-20 21:45:39 +0530

    fix: update the deployment script

--- tasks/deploy-dia-data.ts
+++ tasks/deploy-dia-data.ts
@@ -4,14 +4,14 @@ import { task } from "hardhat/config";
 task(`deploy-dia-data`)
   .addParam("consumeraddress")
   .addParam("defaultKey")
-  .setAction(async ({ consumeraddress, defaultKey }, hre) => {
+  .setAction(async ({ consumeraddress, vaultAddress, defaultKey }, hre) => {
     if (!isAddress(consumeraddress)) throw new Error("invalid address");
     if (defaultKey.length === "") throw new Error("invalid key");
     const { deployments } = hre;
     const [deployer] = await hre.ethers.getSigners();
     const contract = await deployments.deploy("DiaDataAggregator", {
       from: deployer.address,
-      args: [consumeraddress, defaultKey],
+      args: [consumeraddress, vaultAddress, defaultKey],
       skipIfAlreadyDeployed: true,
     });
     console.log(`deployed to`, contract.address);
@@ -19,7 +19,7 @@ task(`deploy-dia-data`)
     if (process.env.NODE_ENV != "test") {
       await hre.run("verify:verify", {
         address: contract.address,
-        constructorArguments: [consumeraddress, defaultKey],
+        constructorArguments: [consumeraddress, vaultAddress, defaultKey],
       });
-    }else console.log(`Contract not verified, deployed locally.`);
+    } else console.log(`Contract not verified, deployed locally.`);
   });
